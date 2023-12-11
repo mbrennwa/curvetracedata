4cec58491579a21228abf2d833cfde1322dccb9e
Matthias Brennwald <matthias@brennwald.org>
2023-12-11 21:51:15 +0100

    .

--- FQA19N20/proc.sh
+++ FQA19N20/proc.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-curveprocess data/*.dat --U1I1 [24,1] --nohello > parameters.csv
-curvematch data/*.dat --U1range [5,26] --I1range [0,5] --nohello > curvematch.csv
-curveplot data/*.dat --pairs --savepdf --nodisplay --xlabel 'Drain-Source Voltage' --ylabel 'Drain Current'
+files=$(find data/*.dat -type f -not -name "*NA*.dat" | sort)
+
+echo Calculating parameters...
+curveprocess $files --U1I1 [24,1] --nohello >parameters.csv
+
+echo Matching curves...
+curvematch $files --U1range [5,26] --I1range [0,5] --nohello >curvematch.csv
+
+echo Plotting curves...
+curveplot $files --pairs --savepdf --nodisplay --xlabel 'Drain-Source Voltage' --ylabel 'Drain Current' --fontname Arial
 mv *blue*.pdf plots/
+
+echo Generating HTML files...
+csvtotable parameters.csv parameters.html --caption "FQA19N20 Parameters" --overwrite
+csvtotable curvematch.csv curvematch.html --caption "FQA19N20 Curve Matching" --overwrite
+cp ../_tools/index_template.html ./
+mv index_template.html index.html
+sed -i 's/TEMPLATE/FQA19N20/g' index.html
+
+echo Done.
