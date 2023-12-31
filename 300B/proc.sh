fa230f1b28c685b997b42e71b75df8db4cf7d757
Matthias Brennwald <matthias@brennwald.org>
2023-12-31 12:27:21 +0100

    reproc

--- 300B/proc.sh
+++ 300B/proc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-../_tools/proc.sh 300B 'Anode-Cathode Voltage' 'Anode Current' 500 0.1 40 m 300 0.1 100 400 0.01 0.1
+s
+../_tools/proc.sh 300B 'Anode-Cathode Voltage' 'Anode Current' 500 0.1 40 m 14 6 300 0.1 100 400 0.01 0.1 0
