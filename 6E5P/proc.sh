11cd60a228f42b8726c64ec185bd880f1c954bf7
Matthias Brennwald <mbrennwa@gmail.com>
2024-02-04 16:55:14 +0100

    up

---
+++ 6E5P/proc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# DEVICENAME: name of the DUT/device"
+# XLABEL: x-axis label for the curve plots"
+# YLABEL: y-axis label for the curve plots"
+# XLIMIT: x-axis limit for the curve plots"
+# YLIMIT: y-axis limit for the curve plots"
+# XYLIMIT: x*y (power) limit for the curve plots"
+# YSCALE: y-axis scale ('0', 'm','mu', etc.)"
+# WIDTH: plot width"
+# HEIGHT: plot height"
+# U0 and I0: voltage/current value where the device parameters are determined (values in V and A)"
+# U1--U2 and I1--I2: voltage/current range used for curve matching (values in V and A)"
+# VBE: if VBE > 0 data is processed assuming a BJT DUT with the specified Vbe voltage (value in V)"
+
+../_tools/proc.sh 6E5P 'Anode-Cathode Voltage' 'Anode Current' 400 0.08 3 m 14 6 200 0.03 100 300 0.005 0.06 0
