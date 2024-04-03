af8cdcae0b7117a4161bba6be48364f9d9b27841
Matthias Brennwald <mbrennwa@gmail.com>
2024-04-03 20:23:57 +0200

    up

---
+++ 10_VT25/proc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# DEVICENAME: name of the DUT/device"
+# XLABEL: x-axis label for the curve plots"
+# YLABEL: y-axis label for the curve plots"
+# XLIMIT: x-axis limit for the curve plots"
+# YLIMIT: y-axis limit for the curve plots"
+# XYLIMIT: x*y (power) limit for the curve plots"
+# YSCALE: y-axis scale ('0', 'm','mu', etc.)"
+# WIDTH: plot width"
+# HEIGHT: plot height"
+# U0 and I0: voltage/current value where the device parameters are determined (values in V and A)"
+# U1--U2 and I1--I2: voltage/current range used for curve matching (values in V and A)"
+# VBE: if VBE > 0 data is processed assuming a BJT DUT with the specified Vbe voltage (value in V)"
+
+../_tools/proc.sh VT25 'Anode-Cathode Voltage' 'Anode Current' 450 0.028 20 m 14 6 300 0.02 50 450 0.001 0.03 0
