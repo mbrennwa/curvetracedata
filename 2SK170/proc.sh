1270d48daf559f31015d3c44880b9dcf9415b50d
Matthias Brennwald <mbrennwa@gmail.com>
2024-04-03 20:21:22 +0200

    up

---
+++ 2SK170/proc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# DEVICENAME: name of the DUT/device"
+# XLABEL: x-axis label for the curve plots"
+# YLABEL: y-axis label for the curve plots"
+# XLIMIT: x-axis limit for the curve plots"
+# YLIMIT: y-axis limit for the curve plots"
+# XYLIMIT: x*y (power) limit for the curve plots"
+# YSCALE: y-axis scale ('0', 'm','mu', etc.)"
+# WIDTH: plot width"
+# HEIGHT: plot height"
+# U0 and I0: voltage/current value where the device parameters are determined (values in V and A)"
+# U1--U2 and I1--I2: voltage/current range used for curve matching (values in V and A)"
+# VBE: if VBE > 0 data is processed assuming a BJT DUT with the specified Vbe voltage (value in V)"
+
+../_tools/proc.sh 2SK170 'Drain-Source Voltage' 'Drain Current' 18 0.009 20 m 14 6 10 0.004 0 15 0.0 0.008 0
