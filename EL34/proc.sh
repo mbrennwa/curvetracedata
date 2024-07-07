126139cc1cc31b6648184df2017bdf4b79b02798
Matthias Brennwald <mbrennwa@localhost.localdomain>
2024-07-07 16:12:23 +0200

    plots

---
+++ EL34/proc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+s
+../_tools/proc.sh EL34 'Anode-Cathode Voltage' 'Anode Current' 500 0.2 40 m 12 8 350 0.04 0 450 0.0 0.15 0
--- KT66/proc.sh
+++ KT66/proc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 s
-../_tools/proc.sh KT66 'Anode-Cathode Voltage' 'Anode Current' 500 0.2 40 m 14 6 350 0.04 0 450 0.0 0.15 0
+../_tools/proc.sh KT66 'Anode-Cathode Voltage' 'Anode Current' 500 0.2 40 m 12 8 350 0.04 0 450 0.0 0.15 0
