6ca7b419715d436022dac9e00cdec0f4592549f0
Matthias Brennwald <mbrennwa@gmail.com>
2024-07-18 13:20:20 +0200

    cleanup

--- 2SK2087C/process.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curveplot data/*.dat --pairs --xlabel 'Drain-Source Voltage' --ylabel 'Drain Current' --savepdf --nodisplay
-mv *.pdf plots/
-
-echo -e '\n***** Moved PDF files to plots directory'
