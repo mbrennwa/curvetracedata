c4bf3cbc4f23461fe1b6075f7f9802677f3c930a
Matthias Brennwald <matthias@brennwald.org>
2024-12-20 14:41:51 +0100

    add plots

--- 20B/proc.sh
+++ 20B/proc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../_tools/proc.sh 300B 'Anode-Cathode Voltage' 'Anode Current' 600 0.07 25 m 14 6 400 0.04 100 550 0.005 0.06 0
+../_tools/proc.sh 300B 'Anode-Cathode Voltage' 'Anode Current' 600 0.07 25 m 10 6 400 0.04 100 550 0.005 0.06 0
